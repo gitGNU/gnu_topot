c5c6c3875ff86fddd474e1cf6a34939519b908c9
Marijn Haverbeke <marijn@ko3moc.(none)>
2007-10-18 20:53:10 +0200

    add primitives and tickers

--- transform.py
+++ transform.py
@@ -17,3 +17,33 @@ def sticky(signal):
       state[0] = not state[0]
     return state[0]
   return Signal(flip, signal)
+
+def primitive(signal, initstate = 0, minval = None, maxval = None):
+  def limit(value):
+    if not minval is None:
+      value = max(minval, value)
+    if not maxval is None:
+      value = min(maxval, value)
+    return value
+
+  def change(value):
+    return limit(value + signal.value)
+  return Signal(change, signal, init = initstate)
+      
+class Ticker:
+  def __init__(self, frequency):
+    self.frequency = frequency
+    self.signal = InputSignal(False)
+
+  def start(self, topot):
+    topot.registerInput("tick", lambda : self.signal)
+    return self.tick()
+  def tick(self):
+    while True:
+      yield ("wait", 1.0/self.frequency)
+      self.signal.value = not self.signal.value
+
+def repeat(ticks, signal):
+  result = Signal(lambda : signal.value, ticks)
+  result.stable = False
+  return result
